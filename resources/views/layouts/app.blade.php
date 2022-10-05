7610b0bf3d57bb66889a9172027f3633cb3e1196
jltkd <jl@turnkeydigital.com>
2022-10-05 12:54:57 -0500

    Added meta description into header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Place to add what I am learning.">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
